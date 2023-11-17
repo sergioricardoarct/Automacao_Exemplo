3a528d30e3f5bd68717bf585f63f120112f45f22
sergio <sergioricardo.arct@gmail.com>
2023-11-17 13:56:14 -0300

    Automação de teste - S - 17-11-2023 1356

---
+++ Exercícios Básicos/001_Bibliotecas.py
@@ -0,0 +1,11 @@
+
+### Bibliotecas ###
+
+
+# As principais bibliotecas que usamos são as Selenium e a Pytest.
+
+# Para a instalação utiliza-se as linhas de comando, no terminal do seu ambiente.
+
+# pip install selenium
+# pip install pytest
+
